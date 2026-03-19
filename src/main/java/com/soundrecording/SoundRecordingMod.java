760872d6337c9e1bbc7278420e52466e9eda41d1
ccm <38798120ccm@gmail.com>
2026-03-20 02:42:24 +0800

    Beta v0.1

--- src/main/java/com/soundrecording/SoundRecordingMod.java
+++ src/main/java/com/soundrecording/SoundRecordingMod.java
@@ -14,7 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SoundRecordingMod implements ModInitializer {
-	public static final String MOD_ID = "soundrecording-mod";
+	public static final String MOD_ID = "soundrecording";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
